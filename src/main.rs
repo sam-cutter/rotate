31d86dea6f8183e242985513eb64e20c2b7e0a61
alexanderFLower-10 <alexanderfowlerwork@gmail.com>
2026-03-01 20:45:14 +0000

    done

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,12 @@ fn main() {
     let hours_flat: Vec<&Hour> = hours.iter().flat_map(|day| day.iter()).collect();
     let people: Vec<Person> = Vec::new();
     let roles: Vec<RoleId> = Vec::new();
+    let blacklist_employee_pairs: HashSet<(PersonId, PersonId)> = HashSet::new();
 
     let mut variables = ProblemVariables::new();
-
     let mut assigned: HashMap<(HourId, PersonId), Variable> = HashMap::new();
     let mut assigned_to_period: HashMap<(HourId, u32, PersonId), Variable> = HashMap::new();
+    let mut assigned_pairs: HashMap<(PersonId, PersonId, HourId), Variable> = HashMap::new();
 
     // Create the assignment variables.
     for &hour in &hours_flat {
@@ -37,7 +38,16 @@ fn main() {
             assigned.insert((hour.id(), person.id()), variables.add(variable().binary()));
         }
     }
-
+    for person_a in &people {
+        for person_b in &people {
+            for hour in &hours_flat {
+                assigned_pairs.insert(
+                    (person_a.id(), person_b.id(), hour.id()),
+                    variables.add(variable().binary()),
+                );
+            }
+        }
+    }
     // Create the variables which indicate if someone is assigned for a certain period.
     for person in &people {
         for day in &hours {
@@ -212,6 +222,24 @@ fn main() {
         }
     }
 
+    for hour in &hours_flat {
+        for person_a in &people {
+            for person_b in &people {
+                let paired: Variable = assigned_pairs[&(person_a.id(), person_b.id(), hour.id())];
+                let a_assigned: Variable = assigned[&(hour.id(), person_a.id())];
+                let b_assigned: Variable = assigned[&(hour.id(), person_b.id())];
+                model.add_constraint(constraint!(paired <= a_assigned));
+                model.add_constraint(constraint!(paired <= b_assigned));
+                model.add_constraint(constraint!(paired >= a_assigned + b_assigned - 1));
+                if blacklist_employee_pairs.contains(&(person_a.id(), person_b.id()))
+                    || blacklist_employee_pairs.contains(&(person_b.id(), person_a.id()))
+                {
+                    model.add_constraint(constraint!(paired == 0));
+                }
+            }
+        }
+    }
+
     let solution = model.solve().unwrap();
 
     for (day_index, day) in hours.iter().enumerate() {
@@ -229,4 +257,155 @@ fn main() {
             println!("{}", assigned.join(", "));
         }
     }
+}
+
+fn test_data() -> (
+    Vec<Vec<Hour>>,
+    HashSet<(PersonId, PersonId)>,
+    Vec<Person>,
+    Vec<RoleId>,
+    HashMap<PersonId, String>,
+) {
+    // Return (hours, blacklist_employee_pairs, people, roles, person_id_to_name)
+    // id 9 is monday 9am to 10am
+    // id 220 is wednesday (as 2 in hunderds so 2 days after monday) from 8:00 pm to 9:00 pm
+    // role 0 is only role
+    let mut hours: Vec<Vec<Hour>> = vec![vec![]];
+    let hour_id_to_needed_staff: HashMap<HourId, u32> = vec![
+        (9, 2),
+        (10, 3),
+        (11, 4),
+        (12, 5),
+        (13, 5),
+        (14, 5),
+        (15, 5),
+        (16, 4),
+        (17, 4),
+        (18, 6),
+        (19, 6),
+        (20, 6),
+        (21, 3),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+    for hour_id in 9..=21 {
+        hours[0].push(Hour::new(
+            (hour_id) as u32,
+            vec![(0, hour_id_to_needed_staff[&hour_id])]
+                .iter()
+                .cloned()
+                .collect(),
+            7.0,
+        ))
+    }
+    // blacklist alex id 0 and shay id 1
+    let blacklist_employee_pairs: HashSet<(PersonId, PersonId)> = HashSet::new();
+
+    let person_id_to_name: HashMap<PersonId, String> = vec![
+        (0, String::from("CheapM1")),
+        (1, String::from("CheapM2")),
+        (2, String::from("CheapM3")),
+        (3, String::from("ExpM1")),
+        (4, String::from("ExpM2")),
+        (5, String::from("ExpM3")),
+        (6, String::from("ExpNNM1")),
+        (7, String::from("ExpNNM2")),
+        (8, String::from("ExpNN3")),
+        (9, String::from("CheapE1")),
+        (10, String::from("CheapE2")),
+        (11, String::from("CheapE3")),
+        (12, String::from("ExpE1")),
+        (13, String::from("ExpE2")),
+        (14, String::from("ExpE3")),
+        (15, String::from("ExpNNE1")),
+        (16, String::from("ExpNNE2")),
+        (17, String::from("ExpNNE3")),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+    let mut people: Vec<Person> = Vec::new();
+
+    // 0 to 2 are cheap, min 2 hours, morning team
+    for person_id in 0..=2 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            2,
+            9.45,
+            vec![9, 10, 11, 12, 13, 14, 15].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    // expensive, min 2 hours, morning team
+    for person_id in 3..=5 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            2,
+            11.45,
+            vec![9, 10, 11, 12, 13, 14, 15].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    // expensive, min 0 hours, morning team
+    for person_id in 6..=8 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            0,
+            11.45,
+            vec![9, 10, 11, 12, 13, 14, 15].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    // cheap, min 2 hours, night team
+    for person_id in 9..=11 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            2,
+            9.45,
+            vec![16, 17, 18, 19, 20, 21].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    // expensive, min 2 hours, night team
+    for person_id in 12..=14 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            2,
+            11.45,
+            vec![16, 17, 18, 19, 20, 21].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    // expensive, min 0 hours, morning team
+    for person_id in 15..=17 {
+        people.push(Person::new(
+            person_id,
+            0,
+            8,
+            0,
+            11.45,
+            vec![16, 17, 18, 19, 20, 21].iter().cloned().collect(),
+            10.0,
+        ))
+    }
+    let roles: Vec<RoleId> = vec![0];
+    // Return (hours, blacklist_employee_pairs,  people, roles, person_id_to_name)
+    (
+        hours,
+        blacklist_employee_pairs,
+        people,
+        roles,
+        person_id_to_name,
+    )
 }
